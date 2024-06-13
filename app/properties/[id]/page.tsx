f04478ebf00d82a31122941e51a04d43c7ac56c4
mujeebcodes <mailmujeeb@icloud.com>
2024-06-13 21:28:54 +0100

    reviews funtionaility implemented

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,9 +8,12 @@ import ImageContainer from "@/components/properties/ImageContainer";
 import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
 import UserInfo from "@/components/properties/UserInfo";
+import PropertyReviews from "@/components/reviews/PropertyReviews";
+import SubmitReview from "@/components/reviews/SubmitReview";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
-import { fetchProperyDetails } from "@/utils/actions";
+import { fetchProperyDetails, findExistingReview } from "@/utils/actions";
+import { auth } from "@clerk/nextjs/server";
 import dynamic from "next/dynamic";
 import { redirect } from "next/navigation";
 
@@ -28,6 +31,12 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
   const profileImage = property.profile.profileImage;
+
+  const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
+
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -56,6 +65,9 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
           <BookingCalendar />
         </div>
       </section>
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+
+      <PropertyReviews propertyId={property.id} />
     </section>
   );
 };
