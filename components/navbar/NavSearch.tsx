9828d3053df55e9141817b2115eff6bf3db2cbe8
mujeebcodes <mailmujeeb@icloud.com>
2024-06-13 12:13:45 +0100

    fixed nav search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 const NavSearch = () => {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
 
   const [search, setSearch] = useState(
@@ -21,7 +20,7 @@ const NavSearch = () => {
       params.delete("search");
     }
 
-    replace(`${pathname}?${params.toString()}`);
+    replace(`/?${params.toString()}`);
   }, 500);
 
   useEffect(() => {
