c5d6de96e591f44e0ec51857ea825f08156ea0e9
mujeebcodes <mailmujeeb@icloud.com>
2024-06-13 12:01:45 +0100

    created the properties page and incorporated maps with leaflet

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,13 +1,23 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import Amenities from "@/components/properties/Amenities";
 import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import Description from "@/components/properties/Description";
 import ImageContainer from "@/components/properties/ImageContainer";
 import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
 import UserInfo from "@/components/properties/UserInfo";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
 import { fetchProperyDetails } from "@/utils/actions";
+import dynamic from "next/dynamic";
 import { redirect } from "next/navigation";
+
+const DynamicMap = dynamic(
+  () => import("@/components/properties/PropertyMap"),
+  { ssr: false, loading: () => <Skeleton className="h-[400px] w-full" /> }
+);
 
 const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
   const property = await fetchProperyDetails(params.id);
@@ -37,6 +47,10 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
           </div>
           <PropertyDetails details={details} />
           <UserInfo profile={{ firstName, profileImage }} />
+          <Separator className="mt-4" />
+          <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
