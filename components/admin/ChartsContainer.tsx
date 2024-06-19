07d5082b7d3026239d35081ab36241980bae405a
mujeebcodes <mailmujeeb@icloud.com>
2024-06-19 16:54:03 +0100

    admin page completed with charts

--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,4 +1,12 @@
-const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+import { fetchChartsData } from "@/utils/actions";
+import Chart from "./Chart";
+
+const ChartsContainer = async () => {
+  const bookings = await fetchChartsData();
+
+  if (bookings.length === 0) {
+    return null;
+  }
+  return <Chart data={bookings} />;
 };
 export default ChartsContainer;
